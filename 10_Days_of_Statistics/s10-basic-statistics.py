31f1f9768d5956f6781b0ba4d8f2a51079bebb18
serler <serler@ptc.com>
2020-03-21 18:39:57 +0100

    update

---
+++ 10_Days_of_Statistics/s10-basic-statistics.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n = int(input())
+    print(n)
+    input_string = input()
+    numbers = list(map(int, input_string.split()))
+    print (numbers)
